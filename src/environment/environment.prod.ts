19a422c053360a75fa5314210d80c5ed61019080
Satyabrata Saha <satyabratasaha20@gmail.com>
2025-02-08 23:24:01 +0530

    first commit

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://banking-portal-api-route-satyabrata-saha-dev.apps.rm3.7wse.p1.openshiftapps.com/api',
+  tokenName :  "authToken",
+  origin:'http://localhost:4200' 
+};
